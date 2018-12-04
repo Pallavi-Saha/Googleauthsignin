71ea05ba48ec1d9a8d287146345362ddc79874b3
Pallavi-Saha <41776280+Pallavi-Saha@users.noreply.github.com>
2018-12-05 03:01:34 +0530

    Update MainActivity.java

--- app/src/main/java/com/scholar/googleauthsignin/MainActivity.java
+++ app/src/main/java/com/scholar/googleauthsignin/MainActivity.java
@@ -3,13 +3,11 @@ package com.scholar.googleauthsignin;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
